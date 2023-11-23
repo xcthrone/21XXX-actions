eb4544ce59dc9cb6b390e3328ecbfbb812af0754
li.peng5 <2435989634@qq.com>
2023-11-23 11:24:01 +0800

    demo

---
+++ src/test/java/SumTwoNumberTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author :xc-throne
+ * @date : 2023/11/23
+ * @description: 单元测试
+ */
+
+
+public class SumTwoNumberTest {
+
+    @Test
+    public void testTwoSum(){
+        SumTwoNumber sumTwoNumber = new SumTwoNumber();
+        int[] nums1 = new int[]{2, 7, 11, 15};
+        int target1 = 9;
+        Assert.assertArrayEquals(new int[]{0,1},sumTwoNumber.twoSum(nums1,target1));
+    }
+
+}
